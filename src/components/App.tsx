1761da6901d07cefe480fa44215e21fc0cacbc20
Beth <bethan.hp@gmail.com>
2023-07-25 15:40:29 +0100

    completed level 350 & moved filter function

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,25 +1,29 @@
-import Episode from "./Episode";
+import { Episode, EpisodeInfo } from "./Episode";
+import { search } from "./episodeUtils";
 import "./style.css";
-import episodes from "../episodes.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+let count = 0;
 
 function App() {
     const [typedSearch, setTypedSearch] = useState("");
     const handleSearch = (searchWord: string) => setTypedSearch(searchWord);
+    const [episodes, setEpisodes] = useState<EpisodeInfo[]>([]);
 
-    let filteredData;
-    if (typedSearch.length > 0) {
-        filteredData = episodes.filter(
-            (ep) =>
-                ep.name.toLowerCase().includes(typedSearch.toLowerCase()) ||
-                (ep.summary !== null &&
-                    ep.summary
-                        .toLowerCase()
-                        .includes(typedSearch.toLowerCase()))
-        );
-    } else {
-        filteredData = episodes;
-    }
+    useEffect(() => {
+        const getEpisodes = async () => {
+            const response = await fetch(
+                "https://api.tvmaze.com/shows/82/episodes"
+            );
+            const jsonBody: EpisodeInfo[] = await response.json();
+            console.log(count++);
+
+            setEpisodes(jsonBody);
+        };
+        getEpisodes();
+    }, []);
+
+    const filteredData = search(typedSearch, episodes);
 
     const episodesOutput = filteredData.map((eachEp, index) => (
         <Episode key={index} episode={eachEp} />
