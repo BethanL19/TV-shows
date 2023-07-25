a50704fd56067db1faf82b4f7f930923ba22fbb5
Beth <bethan.hp@gmail.com>
2023-07-25 12:13:03 +0100

    level 200 search bar added

--- src/components/episodeUtils.test.ts
+++ src/components/episodeUtils.test.ts
@@ -1,4 +1,4 @@
-import { padNumber, removeTags } from "./episodeUtils";
+import { padNumber, removeTags, search } from "./episodeUtils";
 
 test("test padNumber function", () => {
     expect(padNumber(7)).toBe("07");
@@ -8,4 +8,17 @@ test("test padNumber function", () => {
 test("test removeTags function", () => {
     expect(removeTags("<p>Hi I'm Beth</p>")).toBe("Hi I'm Beth");
     expect(removeTags("Hi I'm <p></p>Rosie")).toBe("Hi I'm Rosie");
+});
+
+test("test filtering function", () => {
+    expect(
+        search("three", [
+            { name: "one two three", summary: "one" },
+            { name: "one two", summary: "three" },
+            { name: "one", summary: "one" },
+        ])
+    ).toStrictEqual([
+        { name: "one two three", summary: "one" },
+        { name: "one two", summary: "three" },
+    ]);
 });
--- src/components/episodeUtils.ts
+++ src/components/episodeUtils.ts
@@ -4,3 +4,18 @@ export const padNumber = (num: number): string =>
 export function removeTags(str: string): string {
     return str.replace(/<[^>]*>/g, "");
 }
+
+type Ep = { name: string; summary: string };
+export function search(typedSearch: string, episodes: Ep[]) {
+    let filteredData;
+    if (typedSearch.length > 0) {
+        filteredData = episodes.filter(
+            (ep) =>
+                ep.name.toLowerCase().includes(typedSearch.toLowerCase()) ||
+                ep.summary.toLowerCase().includes(typedSearch.toLowerCase())
+        );
+    } else {
+        filteredData = episodes;
+    }
+    return filteredData;
+}
