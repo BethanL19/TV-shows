76fc61df99acdd6abe0f01c19aa979ed4e9dd3b5
Beth <bethan.hp@gmail.com>
2023-07-27 15:40:04 +0100

    level 500 complete

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,107 +1,32 @@
-import { Episode, EpisodeInfo } from "./Episode";
-import { display, seasonEpStructure } from "./episodeUtils";
-import "./style.css";
-import { useEffect, useState } from "react";
-import showsRaw from "../shows.json";
+import { useState } from "react";
+import { ShowsPage } from "./ShowsPage";
+import EpisodePage from "./EpisodePage";
 import { ShowInfo } from "../showsInfo";
 
-const shows: ShowInfo[] = showsRaw;
+export function App(): JSX.Element {
+    const [displayShowsPage, setDisplayShowPage] = useState(true);
+    const [selectedShow, setSelectedShow] = useState<ShowInfo>();
 
-function App() {
-    const [typedSearch, setTypedSearch] = useState("");
-    const handleSearch = (searchWord: string) => setTypedSearch(searchWord);
-    const [episodes, setEpisodes] = useState<EpisodeInfo[]>([]);
-    const [show, setShow] = useState<ShowInfo>();
-    const [selectedEpisode, setSelectedEpisode] = useState<
-        EpisodeInfo | undefined
-    >();
-
-    const handleShowSelection = (selection: string) => {
-        const showSelectionObj = shows.find((show) => show.name === selection);
-        setShow(showSelectionObj);
+    const handleDisplayChange = (display: boolean) => {
+        setDisplayShowPage(display);
     };
-
-    const handleEpisodeSelection = (selection: string) => {
-        const episodeSelectionObj = episodes.find(
-            (episode) =>
-                `${seasonEpStructure(episode.season, episode.number)} - ${
-                    episode.name
-                }` === selection
-        );
-        setSelectedEpisode(episodeSelectionObj);
+    const handleShowChange = (selection: ShowInfo) => {
+        setSelectedShow(selection);
     };
-
-    useEffect(() => {
-        if (show) {
-            const getEpisodes = async () => {
-                const response = await fetch(
-                    `https://api.tvmaze.com/shows/${show.id}/episodes`
-                );
-                const jsonBody: EpisodeInfo[] = await response.json();
-                setEpisodes(jsonBody);
-            };
-            getEpisodes();
-        }
-    }, [show]);
-
-    const filteredData = display(typedSearch, episodes, selectedEpisode);
-
-    const episodesOutput = filteredData.map((eachEp, index) => (
-        <Episode key={index} episode={eachEp} />
-    ));
-
-    const countOfEps = () => {
-        const totalEps: number = episodes.length;
-        const displayedEps: number = filteredData.length;
-        return "Displaying " + displayedEps + " / " + totalEps + " episodes";
-    };
-
-    const sortedShows = shows.sort((a, b) => a.name.localeCompare(b.name));
-    const showTitles = sortedShows.map((eachShow, index) => (
-        <option key={index}>{eachShow.name}</option>
-    ));
-
-    const episodeTitles = episodes.map((eachEpisode, index) => (
-        <option key={index}>{`${seasonEpStructure(
-            eachEpisode.season,
-            eachEpisode.number
-        )} - ${eachEpisode.name}`}</option>
-    ));
 
     return (
         <>
-            <select
-                name="shows"
-                id="shows-select"
-                onChange={(event) => {
-                    handleShowSelection(event.target.value);
-                }}
-            >
-                <option value="">-- Please select a show --</option>
-                {showTitles}
-            </select>
-            <select
-                name="episodes"
-                id="episodes-select"
-                onChange={(event) => {
-                    handleEpisodeSelection(event.target.value);
-                }}
-            >
-                <option value="">-- Please select an episode --</option>
-                {episodeTitles}
-            </select>
-            <input
-                className="searchBar"
-                placeholder="Search..."
-                value={typedSearch}
-                onChange={(event) => {
-                    handleSearch(event.target.value);
-                }}
-            />
-            <div className="countOfEps">{countOfEps()}</div>
-            <div className="App">{episodesOutput}</div>;
+            {displayShowsPage === false && selectedShow !== undefined ? (
+                <EpisodePage
+                    show={selectedShow}
+                    displayShowPage={handleDisplayChange}
+                />
+            ) : (
+                <ShowsPage
+                    clickedShow={handleShowChange}
+                    displayShowPage={handleDisplayChange}
+                />
+            )}
         </>
     );
 }
-
-export default App;
